f83360caba1d2ce3df3157f73535eec42289674c
harutyunmkhitaryan <harutyun.mkhitaryan@ameriabank.am>
2025-10-09 10:35:12 +0400

    fix: fixed storybook styles issues

--- packages/storybook/.storybook/preview.ts
+++ packages/storybook/.storybook/preview.ts
@@ -1,3 +1,5 @@
+import '@ab.uitools/ui-library/assets/styles/index.scss';
+
 export const parameters = {
   designToken: {
     defaultTab: 'Colors',
--- packages/storybook/src/index.ts
+++ packages/storybook/src/index.ts
@@ -1,3 +1,1 @@
-import './assets/styles/index.scss';
-
 export { useFieldArray } from 'react-hook-form';
