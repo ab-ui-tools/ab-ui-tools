c61afb8b6c529b8c848f897be518147d8103be2b
harutyunmkhitaryan <harutyun.mkhitaryan@ameriabank.am>
2025-12-04 08:52:22 +0400

    fix: test

--- packages/ui-library/src/components/AdvancedTable/hooks/useTableControl.tsx
+++ packages/ui-library/src/components/AdvancedTable/hooks/useTableControl.tsx
@@ -39,6 +39,7 @@ export function useTableControl<TData>({
   const shouldPersistToStorage = tableSettings?.persistColumnSettings === STORAGE_TYPE.LOCAL;
   const shouldEmitExternal = tableSettings?.persistColumnSettings === STORAGE_TYPE.EXTERNAL;
   const tableName = `${TABLE_NAME_PREFIX}-${tableSettings?.tableName}`;
+
   const savedSettings =
     shouldPersistToStorage && tableName
       ? loadTableSettings(tableName)
