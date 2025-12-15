69641370af4a27147c1746ef6ef16c4c0f6c057b
harutyunmkhitaryan <harutyun.mkhitaryan@ameriabank.am>
2025-12-15 13:22:06 +0400

    fix: deleted js files from scripts

--- packages/scripts/src/common/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.Script = void 0;
-var Script;
-(function (Script) {
-    Script["Init"] = "init";
-})(Script = exports.Script || (exports.Script = {}));
