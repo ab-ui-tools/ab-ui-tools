544e8ba928c627ced08b7a9f02d9875778e07045
Garik Grigoryan <garikgrigoryan@Gariks-MacBook-Pro.local>
2026-03-24 17:15:25 +0400

    fix: select prop type

--- packages/ui-library/src/components/Select/types.ts
+++ packages/ui-library/src/components/Select/types.ts
@@ -132,7 +132,7 @@ export interface IRenderOptionItemProps extends TSelectItemProps {
   ref?: Ref<HTMLDivElement>;
 }
 
-export interface TSingleSelectPropTypes<T> extends IFormCompProps, TSelectBaseProps {
+export interface TSingleSelectPropTypes extends IFormCompProps, TSelectBaseProps {
   options: TSelectOptions;
   selectedItem?: TItemValue;
   setSelectedItem?: (items: TItemValue | undefined) => void;
@@ -147,7 +147,7 @@ export interface TSingleSelectPropTypes<T> extends IFormCompProps, TSelectBasePr
   tooltipAddons?: TTooltipProps;
   isCreateOnOutsideClick?: boolean;
   isAllowed?: (values: { formattedValue: string; value: string; floatValue: number | undefined }) => boolean;
-  renderOptions?: (props: IRenderOptionItemProps | T) => JSX.Element;
+  renderOptions?: (props: IRenderOptionItemProps) => JSX.Element;
   defaultValue?: string;
   shouldRecalculateDropdownPosition?: boolean;
 }
