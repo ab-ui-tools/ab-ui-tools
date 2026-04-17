ae32d01b58df1e10677e31a849fbb3672873aacd
Garik Grigoryan <garikgrigoryan@Garik-Grigoryan-Mac.local>
2026-04-17 11:12:09 +0400

    feat: create new select componrnt with react select package

--- packages/base/rollup.config.js
+++ packages/base/rollup.config.js
@@ -1,5 +1,5 @@
 import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
 import copy from 'rollup-plugin-copy'
 import fs from 'fs'
