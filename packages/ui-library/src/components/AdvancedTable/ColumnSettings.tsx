5a835dde8cb13c237fb8e04193cb26a371bb1537
Ramella.Naltakyan <Ramella.Naltakyan@ameriabank.am>
2026-06-04 16:11:33 +0400

    fix: lint fix

--- packages/ui-library/src/components/AdvancedTable/ColumnSettings.tsx
+++ packages/ui-library/src/components/AdvancedTable/ColumnSettings.tsx
@@ -2,13 +2,14 @@ import type { Column, Table } from '@tanstack/react-table';
 
 import { useState } from 'react';
 
+import type { TButtonPropTypes } from '../Button/types';
+
 import { Positions } from '../Tooltip/types';
 import { Tooltip } from '../Tooltip';
 import { Switcher } from '../Switcher';
 import { IconSettings } from '../SVGIcons';
 import { Menu } from '../Menu';
 import { Button } from '../Button';
-import type { TButtonPropTypes } from "../Button/types";
 
 interface ColumnSettingsProps<T> {
   table: Table<T>;
@@ -26,12 +27,10 @@ export function ColumnSettings<T>({
   tooltipText,
   menuIconTooltipText,
   hiddenColumns = [],
-  allToggleText = "All",
+  allToggleText = 'All',
   buttonProps,
 }: ColumnSettingsProps<T>) {
-  const [menuButtonRef, setMenuButtonRef] = useState<HTMLButtonElement | null>(
-    null
-  );
+  const [menuButtonRef, setMenuButtonRef] = useState<HTMLButtonElement | null>(null);
   const [ref, setRef] = useState<HTMLDivElement | null>(null);
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
@@ -46,17 +45,12 @@ export function ColumnSettings<T>({
     }
   };
 
-  const hiddenColumnSettings =
-    defaultHiddenColumnSettings.concat(hiddenColumns);
+  const hiddenColumnSettings = defaultHiddenColumnSettings.concat(hiddenColumns);
 
   return (
     <div ref={setRef}>
       {menuIconTooltipText && (
-        <Tooltip
-          position={Positions.TOP_CENTER}
-          text={menuIconTooltipText}
-          id="column-settings-menu-icon"
-        />
+        <Tooltip position={Positions.TOP_CENTER} text={menuIconTooltipText} id="column-settings-menu-icon" />
       )}
       <Button
         {...buttonProps}
@@ -84,28 +78,19 @@ export function ColumnSettings<T>({
                 selectedValue={table.getIsAllColumnsVisible()}
                 onClick={() => table.toggleAllColumnsVisible()}
                 inlineType={true}
-                size={"small"}
+                size={'small'}
               />
             </div>
           </div>
           <div className="scrollbar--content scrollbar scrollbar--vertical">
-            {table.getAllLeafColumns().map((column) => {
+            {table.getAllLeafColumns().map(column => {
               if (!hiddenColumnSettings?.includes(column.id)) {
                 const label =
-                  typeof column.columnDef.header === "string"
-                    ? column.columnDef.header
-                    : column.columnDef.id;
+                  typeof column.columnDef.header === 'string' ? column.columnDef.header : column.columnDef.id;
                 return (
-                  <div
-                    key={column.id}
-                    className={"settings-menu__dropdown__option"}
-                  >
+                  <div key={column.id} className={'settings-menu__dropdown__option'}>
                     {tooltipText && !column.getCanHide() && (
-                      <Tooltip
-                        position={Positions.TOP_CENTER}
-                        text={tooltipText}
-                        id={column.columnDef.id}
-                      />
+                      <Tooltip position={Positions.TOP_CENTER} text={tooltipText} id={column.columnDef.id} />
                     )}
                     <Switcher
                       label={label}
@@ -114,7 +99,7 @@ export function ColumnSettings<T>({
                       onClick={() => handleClick(column)}
                       disabled={!column.getCanHide()}
                       inlineType={true}
-                      size={"small"}
+                      size={'small'}
                     />
                   </div>
                 );
