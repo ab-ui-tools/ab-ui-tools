decbb83b38ef379c1ce5999ab2ae5d931dd2ffe2
Davit A. Hovhannisyan <davit.hovhannisyan2@ameriabank.am>
2026-08-18 10:15:42 +0400

    fix: added new icons, fixed status component colors

--- packages/ui-library/src/components/Status/consts.ts
+++ packages/ui-library/src/components/Status/consts.ts
@@ -1,12 +1,23 @@
 import type { TTextTypes } from '../Text/types';
+import type { TSVGIconType } from '../SVGIcons/types';
 
-export const COLOR_MAPPING: { [key: string]: TTextTypes } = {
+export const COLOR_MAPPING: Record<TSVGIconType, TTextTypes> = {
   primary: 'primary',
+  secondary: 'secondary',
+  tertiary: 'tertiary',
+  quaternary: 'quaternary',
   disabled: 'disabled',
-  brand: 'brand-light',
-  danger: 'danger-light',
-  warning: 'warning-light',
-  success: 'success-light',
-  information: 'information-light',
-  discovery: 'discovery-light',
+  inverse: 'inverse',
+  'brand-light': 'brand-light',
+  'brand-dark': 'brand-dark',
+  'danger-light': 'danger-light',
+  'danger-dark': 'danger-dark',
+  'warning-light': 'warning-light',
+  'warning-dark': 'warning-dark',
+  'success-light': 'success-light',
+  'success-dark': 'success-dark',
+  'information-light': 'information-light',
+  'information-dark': 'information-dark',
+  'discovery-light': 'discovery-light',
+  'discovery-dark': 'discovery-dark',
 };
